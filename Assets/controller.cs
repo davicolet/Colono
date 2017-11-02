d942b5e714f610f94aaa40814d12d68e4657b9b2
Davi <dcolet@hotmail.com>
2017-11-02 18:32:53 -0200

    first commit

---
+++ Assets/controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+	static public inventario _inventarioController;
+	public GameObject inventario;
+
+	// Use this for initialization
+	void Start () {
+		_inventarioController = inventario.GetComponent<inventario> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
